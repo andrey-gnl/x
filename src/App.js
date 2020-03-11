cadc6ec61ca6458841190d4d49ecc39828bbf098
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2020-03-11 08:07:37 +0200

    widget for top 5 leagues added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.scss';
 import Header from './components/Header/';
 // import GroupList from './components/Group/GroupList';
 import PlayOff from './components/PlayOff/';
+import Sidebar from './components/Sidebar/';
 
 function App() {
   return (
     <div className="page">
-      <div className="page__header">
-        <Header />
+      <div className="page__content">
+        <div className="page__header">
+          <Header />
+        </div>
+        <div className="page__main">
+          <PlayOff />
+        </div>
       </div>
-      <div className="page__main">
-        <PlayOff />
+      <div className="page__sidebar">
+        <Sidebar />
       </div>
     </div>
   );
