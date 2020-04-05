7310889132ef7b66fcc72cc17311b4141c48b403
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2020-04-05 21:04:27 +0300

    reborn: Country component inited

---
+++ src/components/Country/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { withRouter, useParams } from "react-router-dom";
+
+class Country extends Component {
+  render() {
+    let { countryId } = this.props.match.params;
+
+    return (
+        <h2>{countryId}</h2>
+    );
+  }
+}
+
+export default withRouter(Country);
