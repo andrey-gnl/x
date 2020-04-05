ad7ac786a92b3d7bd2cc50296c0b82f1e1390b10
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2020-04-05 21:03:54 +0300

    reborn: club component inited

---
+++ src/components/Clubs/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Clubs extends Component {
+
+  state = {
+    data: [],
+    loaded: false
+  }
+
+  componentDidMount() {
+    fetch(`/api-client/teams/list.json?key=He2aaauPjroKqVba&secret=7jz2KqorkePH30oLerVoCpeVRCFiyQtG`)
+    .then(res => res)
+    .then(({teams}) => {
+      this.setState({data: teams, loaded: true});
+    });
+
+  }
+
+  buildTable() {
+    console.log('hi');
+  }
+
+  render() {
+    const content = this.state.loaded ? this.buildTable() : 'Loading...';
+    return (
+      <div>
+          {content}
+      </div>
+    )
+  }
+}
