70cc4de316652ba23d65eae8da14c2f70ab4cc43
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2020-04-05 21:40:40 +0300

    refactoring: unused removed

--- src/components/Country/index.js
+++ src/components/Country/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withRouter, useParams } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 class Country extends Component {
   render() {
